8e30a10d141ce5ed861c1833d133790a03a676c4
carlitos <carmoicast@gmail.com>
2020-08-14 19:07:27 -0500

    Primer commit

---
+++ config/initializers/inflections.rb
@@ -0,0 +1,6 @@
+ ActiveSupport::Inflector.inflections(:en) do |inflect|
+   inflect.irregular 'activity', 'activities'
+   inflect.irregular 'activity_log', 'activity_logs'
+   inflect.irregular 'assistant', 'assistants'
+   inflect.irregular 'baby', 'babies'
+ end
