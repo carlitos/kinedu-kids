71cb164fced04184e36f2360535bdb40ef4eca27
carlitos <carmoicast@gmail.com>
2020-08-30 00:45:02 -0500

    Formatear fechas, Agregar nombre de la asistente y computar duracion de la actividad

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -4,4 +4,28 @@ class ActivityLog < ApplicationRecord
   belongs_to :baby
   belongs_to :assistant
 
+  after_update :compute_duration
+
+  scope :baby_activities, -> (baby_id) do
+    where('baby_id = ?', baby_id )
+  end
+
+  def date_start
+    start_time.strftime('%Y/%m/%d')
+  end
+
+  def date_end
+    stop_time.strftime('%Y/%m/%d')
+  end
+
+  def assistant_name
+    assistant.name
+  end
+
+
+  def compute_duration
+      self.duration = ( self.stop_time - self.start_time) / 1.minutes
+  end
+
+
 end
