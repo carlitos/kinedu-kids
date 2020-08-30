4f8f4b5fda18a680e8f6031b642e550c0a6f4faf
carlitos <carmoicast@gmail.com>
2020-08-30 00:47:49 -0500

    Agregar mas campos al serializer

--- app/serializer/api/v1/activity_log_serializer.rb
+++ app/serializer/api/v1/activity_log_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::ActivityLogSerializer < Api::V1::BaseSerializer
-  attributes :id, :baby_id, :assistant_id, :activity_id, :duration, :comments
+  attributes :id, :baby_id, :assistant_id, :start_time, :stop_time, :activity_id, :duration, :comments
 end
